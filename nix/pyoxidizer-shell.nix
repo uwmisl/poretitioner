7e2530c943461fc4b891409e291339a6a7f553bb
Jessica Dunstan <jessica.lynn.dunstan@gmail.com>
2020-04-06 17:27:12 -0700

    Basic build, pairing back from here.

--- nix/pyoxidizer-shell.nix
+++ nix/pyoxidizer-shell.nix
@@ -4,7 +4,16 @@ let
   # picked a conservative python version because of errors regarding `cf_feature_version`
   python = pkgs.python37;
   run_dependencies = import ./runDependencies.nix { inherit python; };
-  python_deps = with pkgs; [openssl pkgconfig python] ++ run_dependencies ;
+  python_deps = with pkgs; [
+    openssl
+    pkgconfig
+    pkgs.tk
+    pkgs.tkgate
+    pkgs.tk-8_6
+    pkgs.tk-8_5
+    python
+    python.pkgs.tkinter
+   ] ++ run_dependencies ;
 
   pyoxidizer = pkgs.rustPlatform.buildRustPackage rec {
     pname = "pyoxidizer";
@@ -16,6 +25,28 @@ let
       sha256 = "1n4976wdcdpn4hwm8n7rwwx3pcvlkksad3y44n3q5mmh2lbr21g0";
     };
     cargoSha256 = "0fx0h9wlqbwlfmgz2kq6s3jhlyv32xxrhnmhipsmqb6vfpzm2pbq";
+    buildInputs = [
+      pkgs.libgit2
+      pkgs.tk
+      pkgs.tkgate
+      pkgs.tk-8_6
+      pkgs.tk-8_5
+    ];
+
+    # TODO: Update Apple-only imports with stdenv.isDarwin checks before adding
+    propagatedBuildInputs = [
+      pkgs.darwin.apple_sdk.frameworks.Security
+      pkgs.darwin.apple_sdk.frameworks.AppKit
+      pkgs.darwin.apple_sdk.frameworks.Tcl
+      pkgs.darwin.apple_sdk.libs.Xplugin
+      pkgs.darwin.apple_sdk.frameworks.Quartz
+      pkgs.darwin.apple_sdk.frameworks.QuartzCore
+      pkgs.tk
+      pkgs.tkgate
+      pkgs.tk-8_6
+      pkgs.tk-8_5
+
+    ];
     nativeBuildInputs = python_deps;
     doCheck = false;
   };
@@ -45,7 +76,7 @@ let
 in pkgs.mkShell {
   name = "python-env-shell";
 
-  buildInputs = with pkgs; [ cargo git rustc pyoxidizer ];
+  buildInputs = with pkgs; [ tk tkgate tk-8_6 cargo git rustc pyoxidizer ];
   # shellHook = ''
   #   exec env SSL_CERT_FILE=$SSL_CERT_FILE python-env
   # '';
