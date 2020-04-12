4f15406babb1f1b07a404f1268b2c4b6bc9cac8b
J.D <thequicksort@gmail.com>
2020-04-11 18:37:43 -0700

    Build the application's docker image 🐳 (#14)
    Run "nix-build" or "nix-build -A docker" to build a docker image. Run "nix-build -A app" to build Poretitioner locally without Docker.
    
    https://github.com/uwmisl/poretitioner/pull/14

--- poretitioner/poretitioner.py
+++ poretitioner/poretitioner.py
@@ -1,14 +1,57 @@
-# The following imports are simply a code-review proof-of-concept to illustrate that
-# dependency management works.
-import dask  # noqa: F401
-import jupyter  # noqa: F401
-import numpy  # noqa: F401
-import pandas  # noqa: F401
-
-
 def main():
-    # Stub for the command line
-    print("This print statement is a placeholder.")
+    # The following imports are simply a code-review proof-of-concept to illustrate that
+    # dependency management works.
+
+    print("Importing 'numpy'...")
+    import numpy  # noqa: F401
+
+    print("\tImported 'numpy'!")
+
+    print("Importing 'pandas'...")
+    import pandas  # noqa: F401
+
+    print("\tImported 'pandas'!")
+
+    print("Importing 'h5py'...")
+    import h5py  # noqa: F401
+
+    print("\tImported 'h5py'!")
+
+    print("Importing 'dask'...")
+    import dask  # noqa: F401
+
+    print("\tImported 'dask'!")
+
+    print("Importing 'matplotlib'...")
+    import matplotlib  # noqa: F401
+
+    print("\tImported 'matplotlib'!")
+
+    print("Importing 'seaborn'...")
+    import seaborn  # noqa: F401
+
+    print("\tImported 'seaborn'!")
+
+    print("Importing 'iPython notebook'...")
+    import notebook  # noqa: F401
+
+    print("\tImported 'iPython notebook'!")
+
+    print("Importing 'jupyter'...")
+    import jupyter  # noqa: F401
+
+    print("\tImported 'jupyter'!")
+
+    from matplotlib import pyplot as plt
+
+    print(
+        "As proof that these libraries are properly packaged and callable, drawing a basic plot..."
+    )
+    plt.plot([0, 1], [1, 0])
+    plt.title("matplotlib is properly packaged and importable.")
+    plt.show()
+
+    print("\tPlotted!")
 
 
 if __name__ == "__main__":
