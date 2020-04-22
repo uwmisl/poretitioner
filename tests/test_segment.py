19925b68a842e8351de1fb1c88272dfd7a5a037f
Katie Doroschak <k.doroschak@gmail.com>
2020-04-21 17:36:47 -0700

    add filters (+tests) & find capture fn (untested)

--- tests/test_segment.py
+++ tests/test_segment.py
@@ -105,3 +105,68 @@ def create_capture_fast5_subrun_test():
         assert "sub_run_duration" in list(f5["Meta/tracking_id"].attrs)
         assert f5["Meta/tracking_id"].attrs["sub_run_duration"] == 1000000
     os.remove(capture_f5_fname)
+
+
+def apply_capture_filters_empty_test():
+    """Check for pass when no valid filters are provided."""
+    # capture -- mean: 1; stdv: 0; median: 1; min: 1; max: 1; len: 6
+    capture = [1, 1, 1, 1, 1, 1]
+    filters = {}
+    # No filter given -- pass
+    pass_filters = segment.apply_capture_filters(capture, filters)
+    filters = {"not_a_filter": (0, 1)}
+    # No *valid* filter given -- pass
+    pass_filters = segment.apply_capture_filters(capture, filters)
+    assert pass_filters
+
+
+def apply_capture_filters_length_test():
+    """Test length filter function."""
+    # capture -- mean: 1; stdv: 0; median: 1; min: 1; max: 1; len: 6
+    capture = [1, 1, 1, 1, 1, 1]
+
+    # Only length filter -- pass (edge case, inclusive high)
+    filters = {"length": (0, 6)}
+    pass_filters = segment.apply_capture_filters(capture, filters)
+    assert pass_filters
+
+    # Only length filter -- pass (edge case, inclusive low)
+    filters = {"length": (6, 10)}
+    pass_filters = segment.apply_capture_filters(capture, filters)
+    assert pass_filters
+
+    # Only length filter -- fail (too short)
+    filters = {"length": (8, 10)}
+    pass_filters = segment.apply_capture_filters(capture, filters)
+    assert not pass_filters
+
+    # Only length filter -- fail (too long)
+    filters = {"length": (0, 5)}
+    pass_filters = segment.apply_capture_filters(capture, filters)
+    assert not pass_filters
+
+
+def apply_capture_filters_mean_test():
+    """Test mean filter function. stdv, median, min, and max apply similarly."""
+    # capture -- mean: 0.5; stdv: 0.07; median: 0.5; min: 0.4; max: 0.6; len: 5
+    capture = [0.5, 0.5, 0.6, 0.4, 0.5]
+    # Only mean filter -- pass
+    filters = {"mean": (0, 1)}
+    pass_filters = segment.apply_capture_filters(capture, filters)
+    assert pass_filters
+
+    # Only mean filter -- fail (too high)
+    filters = {"mean": (0, 0.4)}
+    pass_filters = segment.apply_capture_filters(capture, filters)
+    assert not pass_filters
+
+    # Only mean filter -- fail (too low)
+    filters = {"mean": (0.6, 1)}
+    pass_filters = segment.apply_capture_filters(capture, filters)
+    assert not pass_filters
+
+
+def find_captures_test():
+    # TODO: Generate test data : https://github.com/uwmisl/poretitioner/issues/32
+    # See issue for proposed test cases as well
+    assert False
