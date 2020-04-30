34a9179293c3430d61dc7b3f1e537101d5523822
Jessica Dunstan <jdunstan@cs.washington.edu>
2020-04-29 22:17:41 -0700

    Add logging module. Get verbosity and debug mode from command line.

--- poretitioner/poretitioner.py
+++ poretitioner/poretitioner.py
@@ -1,19 +1,64 @@
-import torch  # noqa: F401
-import torchvision  # noqa: F401
-from matplotlib import pyplot as plt
+import argparse
+from typing import Dict
+
+import numpy as np
+from poretitioner.utils.raw_signal_utils import compute_fractional_blockage
+
+from . import logger
 
 
 def main():
-    # The following imports are simply a code-review proof-of-concept to illustrate that
-    # dependency management works.
-    print(
-        "As proof that these libraries are properly packaged and callable, drawing a basic plot..."
-    )
-    plt.plot([0, 1], [1, 0])
-    plt.title("matplotlib is properly packaged and importable.")
-    plt.show()
+    args = get_args()
 
-    print("Plotted!")
+    # Configures the root application logger.
+    logger.configure_root_logger(verbosity=args.verbose, include_debug_info=args.debug)
+
+    log = logger.getLogger()
+    log.debug("A debug message, mostly useful for developers")
+    log.info("A general info message.")
+    log.warning("a warning message, less verbose than just logging info")
+    log.error("An error, this is always logged.")
+    log.fatal("A fatal error - no recovery here. This is always logged.")
+    x = np.array([1.0, 2.3, 4.0, 6.0, 10.0])
+    frac = compute_fractional_blockage(x, 4)
+    print(frac)
+
+
+def get_args() -> Dict:
+    """Gets the command line arguments passed to the application.
+
+    Returns
+    -------
+    Dict
+        Dictionary of command line arguments.
+    """
+
+    # TODO: Add a description string: https://github.com/uwmisl/poretitioner/issues/27
+    DESCRIPTION = ""
+    # TODO: Add a usage string: https://github.com/uwmisl/poretitioner/issues/27
+    USAGE = ""
+
+    # TODO: Add the rest of the commandline arguments and configuration: https://github.com/uwmisl/poretitioner/issues/27
+    parser = argparse.ArgumentParser(description=DESCRIPTION, usage=USAGE)
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Increase the program's verbosity. Can be used multiple times to increase the logging level (e.g. -vvv for very verbose logging)",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        default=False,
+        help="Whether to run in debug mode. Turned off by default.",
+    )
+
+    # TODO: Add the rest of the commandline arguments and configuration: https://github.com/uwmisl/poretitioner/issues/27
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == "__main__":
