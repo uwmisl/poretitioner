2caa4f756bf169fe360928496cb7c0961981e2ff
Jessica Dunstan <jdunstan@cs.washington.edu>
2021-02-19 18:30:27 -0800

    New directory structure: src/

--- poretitioner/__init__.py
+++
@@ -1,8 +0,0 @@
-# import matplotlib
-
-# from . import signals, utils
-# from .poretitioner import main
-
-# # We're using TK as a matplotlib backend since it doesn't require any extra dependencies.
-# # If need a different backend, it can be configured here.
-# matplotlib.use("TkAgg")
