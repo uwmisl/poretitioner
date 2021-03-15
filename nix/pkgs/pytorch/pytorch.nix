d2eeea60c74ae75640d28a12799cf1ddae9cdc5f
Jessica Dunstan <jdunstan@cs.washington.edu>
2021-03-15 11:53:35 -0700

    Pytorch 1.5.1

---
+++ nix/pkgs/pytorch/pytorch.nix
@@ -0,0 +1,24 @@
+###########################################################################################
+#
+# pytorch.nix
+#
+###########################################################################################
+#
+# This provides a stable version of pytorch.
+# Details on how to use it can be found here [1, 2].
+#
+# Why are we building it in manually instead of downloading it from the Nix store?
+# Sadly, it's not in the Nix store yet.
+#
+# [1] - https://github.com/microsoft/debugpy
+# [2] - https://code.visualstudio.com/docs/python/debugging#_command-line-debuggingd
+#
+###########################################################################################
+
+{ pkgs ? import <nixpkgs> { }
+, lib ? pkgs.lib
+, python ?  pkgs.python38
+, cudaSupport ? false
+}:
+with python.pkgs;
+callPackage ./default.nix { inherit python cudaSupport; }
